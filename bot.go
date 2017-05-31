d4898f8e61c72ecb66702e9777324db53a4d6503
csos95 <csoscss@gmail.com>
2017-05-31 14:56:47 -0500

    create commands type

--- bot.go
+++ bot.go
@@ -13,6 +13,8 @@ var bot *Bot
 
 type Bot struct {
 	*discordgo.Session
+	commands []Command
+	help     string
 }
 
 func CreateBot(config *Config) (*Bot, error) {
@@ -21,11 +23,15 @@ func CreateBot(config *Config) (*Bot, error) {
 		return nil, err
 	}
 
-	bot := &Bot{dg}
+	bot := &Bot{Session: dg, commands: make([]Command, 0)}
 
 	bot.AddHandler(ready)
 	bot.AddHandler(messageCreate)
 	bot.AddHandler(guildCreate)
+
+	bot.commands = append(bot.commands, Command{"m!help", "displays this message"})
+
+	bot.createHelp()
 
 	return bot, nil
 }
@@ -58,7 +64,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		parts := strings.Fields(m.Content)
 		switch parts[0] {
 		case "m!help":
-			bot.sendMessage(s, m.ChannelID, helpText())
+			bot.sendMessage(s, m.ChannelID, bot.help)
 		}
 	}
 }
@@ -75,7 +81,9 @@ func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
 	}
 }
 
-func helpText() string {
-	return `Megaprem Bot Help
-m!help: displays this message`
+func (b *Bot) createHelp() {
+	help := "Megaprem Bot Help\n"
+	for _, command := range b.commands {
+		help += fmt.Sprintf("\t%s: %s", command.name, command.description)
+	}
 }
